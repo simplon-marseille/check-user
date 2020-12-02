1038618667e0fb2660299c21d9eb6cd170f86147
simplon-marseille <acardnicolas@hotmail.com>
2020-12-02 23:19:25 +0100

    mise a jour

---
+++ app.js
@@ -0,0 +1,17 @@
+const login = 'avatar200';
+const pwd = '12345678';
+
+//L'application doit recevoir deux entrées - login - mot de passe.
+//=> prompt
+//Si ces deux paramètres sont justes, on affiche 'success'
+//Sinon on affiche 'bad password or login'
+
+
+//Tant le mot de passe ou le login sont faux => refaire la demande au user.
+
+//Le user n'a que 3 essais pour entrer son login et pwd. Au dela,
+//le programme s'arrète, et on affiche 'compte bloqué !'.
+
+
+//Si le user entre une chaine vide
+//Afficher 'Veuillez remplir les champs'
